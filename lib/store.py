ac0db16ff9b5a232edb36403e6b49235ca1b4b68
JC Romero <jcromerohdz@gmail.com>
2015-04-17 11:18:11 -0700

    a ver

--- lib/store.py
+++ lib/store.py
@@ -4,3 +4,4 @@ import redis
 REDISTOGO_URL ='redis://redistogo:23712bcf49adcd0ebf95a130b59d4ed7@mummichog.redistogo.com:10196/'
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
+
